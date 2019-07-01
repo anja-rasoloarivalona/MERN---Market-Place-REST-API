63423e6f02bd0e3fdb5acf6170fe370d84503ef2
Anja <rasoloanja@gmail.com>
2019-07-01 14:47:03 -0400

    Connect MongoDB

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const shopRoutes = require('./routes/shop');
 const app = express();
@@ -17,5 +18,13 @@ app.use(bodyParser.json()); //application/json
 app.use('/', shopRoutes)
 
 
-
-app.listen(8000);
+mongoose
+    .connect('mongodb+srv://anja:anjanirina@cluster0-wijrw.mongodb.net/shop')
+    .then(result =>{
+        console.log('connected')
+        app.listen(8000)
+    })
+    .catch(err => 
+        console.log(err)
+    )
+;

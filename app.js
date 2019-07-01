997178132d61f1dd0b2f50690ad82e989178e8c5
Anja <rasoloanja@gmail.com>
2019-07-01 14:18:50 -0400

    Fixed CORS error

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const shopRoutes = require('./routes/shop');
 const app = express();
@@ -7,15 +8,14 @@ const app = express();
 
 /*--------MIDDLEWARES----------*/
 
+
+app.use('*', cors())
+
+
 app.use(bodyParser.json()); //application/json
 
-app.use((req, res, next) => {
-    res.setHeader('Acces-Control-Allow-Origin', '*');
-    res.setHeader('Acces-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Acces-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-})
-
 app.use('/', shopRoutes)
+
+
 
 app.listen(8000);
--- controllers/shop.js
+++ controllers/shop.js
@@ -1,11 +1,10 @@
 
-
 exports.getIndex = (req, res, next) => {
     res.status(200).json({
         products: [{
             _id: '1',
             title: "First Product",
-            price: '10',
+            price: '12',
             description: 'This is the first product'
         }]
     })

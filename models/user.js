09b770109fa0bde28c80f83af2d933ef33ebc25c
Anja <rasoloanja@gmail.com>
2019-07-04 17:43:51 -0400

    Init user model

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema ({
+    email : {
+        type : String,
+        require: true
+    },
+
+    password: {
+        type: String,
+        required: true
+    },
+
+    name: {
+        type: String,
+        required: true
+    },
+
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    ]
+});
+
+module.exports = mongoose.model('User', userSchema)
